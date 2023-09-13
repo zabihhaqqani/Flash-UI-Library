b310b73eb86f25dc182d2637d93a2103fefb74f3
zabih haqqani <mdzabihhaqqani@gmail.com>
2023-09-13 14:16:24 +0530

    feat: added sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import SideBar from "./components/SideBar/SideBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="main-container">
+        <SideBar />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router-dom";
+import "./SideBar.css";
+
+const SideBar = () => {
+  const getActiveStyle = ({ isActive }) => ({
+    color: isActive && "#f02836",
+  });
+  return (
+    <div>
+      <div className="sidebar-container">
+        <NavLink
+          className="sidebar-items"
+          style={getActiveStyle}
+          to="/installation"
+        >
+          Installation
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/avatar">
+          Avatar
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/alert">
+          Alert
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/badge">
+          Badge
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/button">
+          Button
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/card">
+          Card
+        </NavLink>
+        <NavLink
+          className="sidebar-items"
+          style={getActiveStyle}
+          to="/headings"
+        >
+          Headings
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/text">
+          Text
+        </NavLink>
+        <NavLink className="sidebar-items" style={getActiveStyle} to="/image">
+          Image
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
